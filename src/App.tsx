a70281f0d732fd7ec763a3d3c132f646e0e73628
Junior Batista <Juniorbatistadev@gmail.com>
2024-06-30 20:45:34 -0400

    chore:add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 import Button from "@mui/material/Button";
 
 function App() {
@@ -9,17 +8,16 @@ function App() {
 
   return (
     <>
-      <Button variant="contained">Hello world</Button>;
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <div className="mt-36 bg-slate-800">
+        <a href="https://vitejs.dev" target="_blank" className="mt-16">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <div className="card bg-red-700">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
